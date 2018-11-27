7154564604007bbe88887cd76bde6d2438d65184
Michael Tang <mtang8264@gmail.com>
2018-11-27 11:32:53 -0500

    v1.2.0 - Added screen support
    Stole ScreenTest script from ProgrammingFinal
    Adjusted anchors on UI elements

---
+++ Assets/Scripts/ScreenTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenTest : MonoBehaviour {
+
+	// Use this for initialization
+    void Start ()
+     {
+        Camera camera = GetComponent<Camera>();
+        float aspect = camera.aspect;
+        //iPhone 8
+        if(aspect > 0.55f  && aspect < .57f)
+         {
+            camera.orthographicSize = 4.1f;
+         }
+        //iPhone X
+        else if(aspect > 0.45f && aspect < 0.47f)
+        {
+            camera.orthographicSize = 5f;
+        }
+     }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
