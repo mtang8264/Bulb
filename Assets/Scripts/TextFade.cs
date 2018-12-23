8701d2481882c305fd4983e6a79dc102b198383d
Michael Tang <mtang8264@gmail.com>
2018-12-22 19:59:49 -0500

    v1.3.34 - Added instructions to feed minigame

---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour {
+    Text text;
+    public float secondsTillFade = 1f;
+    public float durationOfFade = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time > secondsTillFade)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(1, 0, (Time.time) / (durationOfFade + secondsTillFade)));
+        }
+	}
+}
